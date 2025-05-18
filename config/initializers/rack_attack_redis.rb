ddae38fc479d7ceb52309128c73b52541eeb2d19
JamieAhale <JamieAhale>
2025-05-18 18:37:48 +1000

    Fixes

--- config/initializers/rack_attack_redis.rb
+++ config/initializers/rack_attack_redis.rb
@@ -1,10 +1,9 @@
 if defined?(Redis) && Rails.env.production?
   # Use Redis as a backing store for Rack::Attack
-  redis_config = {
-    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1'),
+  redis_config = RedisConfig.connection_params.merge(
     namespace: 'rack_attack',
     expires_in: 1.hour
-  }
+  )
 
   # Set cache store for Rack::Attack using ActiveSupport::Cache::RedisCacheStore
   Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(**redis_config)
